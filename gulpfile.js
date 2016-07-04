34eff36def62c4b97f606b76f6c7440874287e59
Andy Tung <andyytung@gmail.com>
2016-07-03 21:10:57 -0700

    setup work environment and folder structure

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+let gulp        = require('gulp');
+let browserSync = require('browser-sync');
+let sass        = require('gulp-sass');
+let jade        = require('gulp-jade');
+let babel       = require('gulp-babel');
+let prefix      = require('gulp-autoprefixer');
+
+
+let messages = {
+    reload: '<span style="color: grey">Gulp:</span> $ reload'
+};
+
+
+gulp.task('reload', () => {
+    browserSync.notify(messages.reload);
+    browserSync.reload();
+});
+
+
+gulp.task('browser-sync', ['sass', 'reload'], () => {
+    browserSync({
+        server: {
+            baseDir: './'
+        },
+        notify: false
+    });
+});
+
+
+gulp.task('sass', () => {
+    return gulp.src('assets/main.scss')
+        .pipe(sass({
+            includePaths: ['stylesheets'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets'));
+});
+
+
+gulp.task('jade', () => {
+  return gulp.src('assets/index.jade')
+  .pipe(jade())
+  .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('js', () => {
+    gulp.src('assets/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('assets'))
+});
+
+
+gulp.task('watch', () => {
+    gulp.watch('assets/**/*.scss', ['sass']);
+    gulp.watch('assets/**/*.jade', ['jade']);
+    gulp.watch('assets/**/*.js', ['js']);
+    gulp.watch(['index.html', '404.html'], ['reload']);
+});
+
+
+gulp.task('default', ['browser-sync', 'sass', 'jade', 'js', 'watch']);
